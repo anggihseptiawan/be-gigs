bcdc3f5ba8808d70cd87dcc388475481cb4bd1e9
Anggih Septiawan <albertanggih@gmail.com>
2021-09-13 22:50:52 +0700

    add flash message in every crud action

--- app/category/controller.js
+++ app/category/controller.js
@@ -3,10 +3,16 @@ const Category = require("./model");
 module.exports = {
 	index: async (req, res) => {
 		try {
+			const alertMessage = req.flash("alertMessage");
+			const alertStatus = req.flash("alertStatus");
+
+			const alert = { message: alertMessage, status: alertStatus };
 			const category = await Category.find();
-			res.render("admin/category/view_category", { category });
+			res.render("admin/category/view_category", { category, alert });
 		} catch (error) {
-			console.log(err);
+			req.flash("alertMessage", `${error.message}`);
+			req.flash("alertStatus", "danger");
+			res.redirect("/category");
 		}
 	},
 
@@ -14,7 +20,9 @@ module.exports = {
 		try {
 			res.render("admin/category/create");
 		} catch (error) {
-			console.log(error);
+			req.flash("alertMessage", `${error.message}`);
+			req.flash("alertStatus", "danger");
+			res.redirect("/category");
 		}
 	},
 
@@ -24,9 +32,13 @@ module.exports = {
 			let category = await Category({ name });
 			await category.save();
 
+			req.flash("alertMessage", "Berhasil tambah kategori");
+			req.flash("alertStatus", "success");
 			res.redirect("/category");
 		} catch (error) {
-			console.log(error);
+			req.flash("alertMessage", `${error.message}`);
+			req.flash("alertStatus", "danger");
+			res.redirect("/category");
 		}
 	},
 
@@ -36,7 +48,9 @@ module.exports = {
 			const category = await Category.findOne({ _id: id });
 			res.render("admin/category/edit", { category });
 		} catch (error) {
-			console.log(error);
+			req.flash("alertMessage", `${error.message}`);
+			req.flash("alertStatus", "danger");
+			res.redirect("/category");
 		}
 	},
 
@@ -45,9 +59,14 @@ module.exports = {
 			const { id } = req.params;
 			const { name } = req.body;
 			await Category.findOneAndUpdate({ _id: id }, { name });
+
+			req.flash("alertMessage", "Berhasil ubah kategori");
+			req.flash("alertStatus", "success");
 			res.redirect("/category");
 		} catch (error) {
-			console.log(error);
+			req.flash("alertMessage", `${error.message}`);
+			req.flash("alertStatus", "danger");
+			res.redirect("/category");
 		}
 	},
 
@@ -55,6 +74,9 @@ module.exports = {
 		try {
 			const { id } = req.params;
 			await Category.findOneAndDelete({ _id: id });
+
+			req.flash("alertMessage", "Berhasil hapus kategori");
+			req.flash("alertStatus", "success");
 			res.redirect("/category");
 		} catch (error) {
 			console.log(eror);
