00d0000e93a6a552747eed73257162a60166c9de
Anggih Septiawan <albertanggih@gmail.com>
2021-09-14 22:03:55 +0700

    start making some api

---
+++ app/player/controller.js
@@ -0,0 +1,45 @@
+const Voucher = require("../voucher/model");
+const Category = require("../category/model");
+
+module.exports = {
+	landingPage: async (req, res) => {
+		try {
+			const voucher = await Voucher.find()
+				.select("_id name status category thumbnail")
+				.populate("category");
+			res.status(200).json({ data: voucher });
+		} catch (error) {
+			res.status(500).json({
+				message: error.message || "Internal server error",
+			});
+		}
+	},
+	detailPage: async (req, res) => {
+		try {
+			const { id } = req.params;
+			const voucher = await Voucher.findOne({ _id: id })
+				.populate("category")
+				.populate("nominals")
+				.populate("user", "_id name phoneNumber");
+
+			if (!voucher) {
+				return res.status(404).json({ message: "voucher not found.." });
+			}
+			res.status(200).json({ data: voucher });
+		} catch (error) {
+			res.status(500).json({
+				message: error.message || "Internal server error",
+			});
+		}
+	},
+	category: async (req, res) => {
+		try {
+			const category = await Category.find();
+			res.status(200).json({ data: category });
+		} catch (error) {
+			res.status(500).json({
+				message: error.message || "Internal server error",
+			});
+		}
+	},
+};
--- app/player/model.js
+++ app/player/model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const HASH_ROUND = 10;
 
 let playerSchema = mongoose.Schema(
 	{
@@ -18,5 +20,24 @@ let playerSchema = mongoose.Schema(
 	},
 	{ timeStamps: true }
 );
+
+playerSchema.path("email").validate(
+	async function (value) {
+		try {
+			const count = await this.model("Player").countDocuments({
+				email: value,
+			});
+			return !count;
+		} catch (error) {
+			throw error;
+		}
+	},
+	(attr) => `${attr.value} sudah terdaftar`
+);
+
+playerSchema.pre("save", function (next) {
+	this.password = bcrypt.hashSync(this.password, HASH_ROUND);
+	next();
+});
 
 module.exports = mongoose.model("Player", playerSchema);
