8736a69a34d63c57c82677c0deca2d3211687c1e
Anggih Septiawan <albertanggih@gmail.com>
2021-09-15 12:58:16 +0700

    make api player profile and edit player profile

--- app/player/controller.js
+++ app/player/controller.js
@@ -4,6 +4,10 @@ const Nominal = require("../nominal/model");
 const Payment = require("../payment/model");
 const Bank = require("../bank/model");
 const Transaction = require("../transaction/model");
+const Player = require("../player/model");
+const path = require("path");
+const fs = require("fs");
+const config = require("../../config");
 
 module.exports = {
 	landingPage: async (req, res) => {
@@ -150,6 +154,141 @@ module.exports = {
 			res.status(500).json({
 				message: error.message || "Internal server error",
 			});
+		}
+	},
+	historyDetail: async (req, res) => {
+		try {
+			const { id } = req.params;
+			const history = await Transaction.findOne({ _id: id });
+
+			if (!history)
+				return res.status(404).json({ message: "History tidak ditemukan" });
+
+			res.status(200).json({ data: history });
+		} catch (error) {
+			res.status(500).json({
+				message: error.message || "Internal server error",
+			});
+		}
+	},
+	dashboard: async (req, res) => {
+		try {
+			const counts = await Transaction.aggregate([
+				{ $match: { player: req.player._id } },
+				{ $group: { _id: "$category", value: { $sum: "$value" } } },
+			]);
+
+			const categories = await Category.find({});
+			categories.forEach((category) => {
+				counts.forEach((count) => {
+					if (count._id.toString() === category._id.toString()) {
+						count.name = category.name;
+					}
+				});
+			});
+
+			const history = await Transaction.find({ player: req.player._id })
+				.populate("category")
+				.sort("updatedAt");
+
+			res.status(200).json({ data: history, counts });
+		} catch (error) {
+			res.status(500).json({
+				message: error.message || "Internal server error",
+			});
+		}
+	},
+	profile: async (req, res) => {
+		try {
+			const player = {
+				id: req.player._id,
+				username: req.player.username,
+				email: req.player.email,
+				name: req.player.name,
+				avatar: req.player.avatar,
+				phoneNumber: req.player.phoneNumber,
+			};
+
+			res.status(200).json({ data: player });
+		} catch (error) {
+			res.status(500).json({
+				message: error.message || "Internal server error",
+			});
+		}
+	},
+	editProfile: async (req, res, next) => {
+		try {
+			const { name = "", phoneNumber = "" } = req.body;
+
+			const payload = {};
+			if (name.length) payload.name = name;
+			if (phoneNumber.length) payload.phoneNumber = phoneNumber;
+
+			if (req.file) {
+				let tmp_path = req.file.path;
+				let originalExt =
+					req.file.originalname.split(".")[
+						req.file.originalname.split(".").length - 1
+					];
+				let filename = req.file.filename + "." + originalExt;
+				let target_path = path.resolve(
+					config.rootPath,
+					`public/uploads/${filename}`
+				);
+
+				const src = fs.createReadStream(tmp_path);
+				const dest = fs.createWriteStream(target_path);
+				src.pipe(dest);
+
+				src.on("end", async () => {
+					let player = await Player.findOne({ _id: req.player._id });
+					let currrentImage = `${config.rootPath}/public/uploads/${player.avatar}`;
+					if (fs.existsSync(currrentImage)) {
+						fs.unlinkSync(currrentImage);
+					}
+
+					player = await Player.findOneAndUpdate(
+						{ _id: req.player._id },
+						{ ...payload, avatar: filename },
+						{ new: true, runValidators: true }
+					);
+
+					res.status(201).json({
+						data: {
+							id: player.id,
+							name: player.name,
+							phoneNumber: player.phoneNumber,
+							avatar: player.avatar,
+						},
+					});
+				});
+
+				src.on("err", async () => {
+					next(err);
+				});
+			} else {
+				const player = await Player.findOneAndUpdate(
+					{ _id: req.player._id },
+					payload,
+					{ new: true, runValidators: true }
+				);
+				res.status(201).json({
+					data: {
+						id: player.id,
+						name: player.name,
+						phoneNumber: player.phoneNumber,
+						avatar: player.avatar,
+					},
+				});
+			}
+		} catch (error) {
+			if (error && error.name === "ValidationError") {
+				res.status(422).json({
+					error: 1,
+					message: error.message,
+					fields: error.errors,
+				});
+			}
 		}
 	},
 };
--- app/player/router.js
+++ app/player/router.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const os = require("os");
 const {
 	landingPage,
 	detailPage,
 	category,
 	checkout,
 	history,
+	historyDetail,
+	dashboard,
+	profile,
+	editProfile,
 } = require("./controller");
 const { isLoginPlayer } = require("../middleware/auth");
 
@@ -14,5 +20,14 @@ router.get("/:id/detail", detailPage);
 router.get("/category", category);
 router.post("/checkout", isLoginPlayer, checkout);
 router.get("/history", isLoginPlayer, history);
+router.get("/history/:id/detail", isLoginPlayer, historyDetail);
+router.get("/dashboard", isLoginPlayer, dashboard);
+router.get("/profile", isLoginPlayer, profile);
+router.put(
+	"/profile",
+	isLoginPlayer,
+	multer({ dest: os.tmpdir() }).single("image"),
+	editProfile
+);
 
 module.exports = router;
